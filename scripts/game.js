50e5f4959aaf25e53a626000a6fd1bc2f7e42749
Katerina <yelkaterina@gmail.com>
2023-05-15 18:07:44 +0200

    Jest testing

---
+++ scripts/game.js
@@ -0,0 +1,25 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ['button1', 'button2', 'button3', 'button4']    
+}
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn()
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns()
+}
+
+const showScore = () => document.getElementById('score').innerText = game.score;
+
+
+module.exports = {game, newGame, showScore, addTurn};
